5c5014164bab8a92fafc17a966acbc5066aaf7fa
Emma <student@emma-matthews-mac-desktop.hub>
2020-09-07 17:28:22 +1200

    Add content and nav bar

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,11 +5,14 @@ const Home = () => {
     <div className='home'>
       <div className="lt-grid-container">
         <div className="lt-Heading">
-          <h1>Heading here</h1>
+          <h1>Love Language</h1>
         </div>
         <div className="lt-Information"></div>
         <div className="lt-NavBar">
-          <h5>Nav bar here</h5>
+          <h5 className='topnav'><a href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a></h5>
         </div>
         <div className="lt-Subheading">
           <h2>Sub heading</h2>
@@ -19,7 +22,7 @@ const Home = () => {
           <h2>Connect to the community</h2>
         </div>
         <div className="lt-Img">
-          <img src="images\cats.png" alt="emma and her cats" />
+          <img src="images\giraffeFlip.png" alt="baby giraffe sketched in pencil" />
         </div>
       </div>
     </div>
